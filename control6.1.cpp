90c2dba675f02e5569585f1ac5ddb86476829b24
Laskin Kirill <72994403+LaskinKirill@users.noreply.github.com>
2021-07-22 16:12:58 +0300

    Add files via upload

---
+++ control6.1.cpp
@@ -0,0 +1,39 @@
+﻿
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+
+void Example1(const char* filename)
+{
+    ofstream os(filename); 
+
+    
+    if (!os)
+    {
+        cout << "Cannot open the file to output. \n";
+        return;
+    }
+
+    char str[80];
+
+    cout << "Save the lines to FILE. Enter ! to stop." << endl;
+
+    do
+    {
+        cout << ":";
+
+        cin.getline(str, 80);
+
+        os << str << endl;
+    } while (*str != '!');
+
+    os.close();
+    return;
+}
+
+void main()
+{
+    Example1("myfile.txt");
+}
+
