54f8bee1e7d201e15189777cea8ae97e0d8caecb
Laskin Kirill <72994403+LaskinKirill@users.noreply.github.com>
2021-07-26 22:05:36 +0300

    Add files via upload

---
+++ control11.2.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm> 
+using namespace std;
+class Point
+{
+
+private:
+	double x;
+	double y;
+public:
+
+	Point(double px, double py) :x(px), y(py)
+	{}
+
+	friend double DistToCenter(Point p)
+	{
+		return  sqrt((p.x) * (p.x) + (p.y) * (p.y));
+	}
+
+	friend bool operator< (const Point& p1, const Point& p2)
+	{
+		return DistToCenter(p1) < DistToCenter(p2);
+	}
+	friend ostream& operator<<(ostream& stream, const Point& p)
+	{
+		stream << "(" << p.x << ", " << p.y << ")";
+		return stream;
+	}
+
+};
+
+
+int main()
+{
+	std::vector<Point> v;
+	v.push_back(Point(11, 21));
+	v.push_back(Point(1, 2));
+	v.push_back(Point(22, 9));
+	v.push_back(Point(14, 18));	sort(v.begin(), v.end());
+	for (auto& point : v)
+		cout << point << '\n';
+	return 0;
+}
