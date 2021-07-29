9f89fbef6e4a9f7b17e00c8e69e973d15432c478
Laskin Kirill <72994403+LaskinKirill@users.noreply.github.com>
2021-07-29 21:28:27 +0300

    Add files via upload

--- control14.cpp
+++ control14.cpp
@@ -1,26 +1,39 @@
+#include <typeinfo>
+using namespace std;
 #include <iostream>
-#include <algorithm>
-#include <vector>
 
-template<typename T>
-double avg(T arr, int n)
+template<class T>
+double average_value(T arr[], int size)
 {
-    int index = 0; double avg_r = 0;
-    while (index < n)
-        avg_r += (double)arr[index++] / n;
-
-    return avg_r;
+	double avg;
+	avg = 0.0;
+	for (int i = 0; i < size; i++)
+		avg += arr[i];
+	avg /= size;
+	return avg;
 }
-
+template<class T>
+void show(T arr[], double avg)
+{
+	cout << "Average value for array type " << typeid(T).name() << "  is:  " << avg << endl;
+}
 int main()
 {
-    int M1[10] = {1,2,3,4,5,6,7,8,9,10 };
-    double M2[10] = {1.3, 1.6, 1.9, 3.8, 6,9, 7.1, 88.22, 33.2, 24.5 };
-    long M3[10] = { 100000000, 20000000, 50000000, 9000000000, 88888888888, 999999999999, 5555555555,77777777777,444444444444,222222222222 };
-    char M4[10] = { 'a', 'b', 'c', 'd', 'g', 'i', 're', 'hg', 'hg', 'dd' };
-
-    printf("avg = %d\n", avg(M1, 10)); // параметр шаблона определяется параметром функции
-    printf("avg = %lf\n", avg(M2, 10));
-    printf("avg = %ld\n", avg(M3, 10));
-    printf("avg = %c\n", avg(M4, 10));
+	int arr[] = { 19,33,17,6,51,47,31,29,12 };
+	double arrd[] = { 2.1, 2.3,1.7,6.6,5.3,2.44,3.1,2.4,1.2 };
+	char arrc[] = "sgfhgjjlkk;l''kjjhg";
+	long arrl[] = { 7247483648, 8114848348,5543278909, 7114748364,-1147483648, -78249483648, -114845348 };
+	int k1 = sizeof(arr) / sizeof(arr[0]);
+	int k2 = sizeof(arrd) / sizeof(arrd[0]);
+	int k3 = sizeof(arrc) / sizeof(arrc[0]) - 1;
+	int k4 = sizeof(arrl) / sizeof(arrl[0]);
+	double avg;
+	avg = average_value(arr, k1);
+	show(arr, avg);
+	avg = average_value(arrd, k2);
+	show(arrd, avg);
+	avg = average_value(arrc, k3);
+	show(arrc, avg);
+	avg = average_value(arrl, k4);
+	show(arrl, avg);
 }
